8c682186134a433ea4c5822fa4a28c6c5ebbce0e
JSNavey <jsnavey@gmail.com>
2018-05-31 14:30:00 -0400

    implemented map method in noteList

--- notes/src/components/NoteList/NoteList.js
+++ notes/src/components/NoteList/NoteList.js
@@ -2,39 +2,50 @@ import React, { Component } from 'react';
 import './index.css';
 
 class NoteList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notesArray: [
+        {
+          _id: 'qazwsx',
+          title: 'First Note',
+          body: 'First lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices.',
+          createdAt: 1527789175361
+        },
+        {
+          _id: 'plmokn',
+          title: 'Second Note',
+          body: 'Second, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices.',
+          createdAt: 1527789175370
+        },
+        {
+          _id: 'ijnuhb',
+          title: 'Third Note',
+          body: 'Third tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices.',
+          createdAt: 1527789175375
+        },
+        {
+          _id: 'edcrfv',
+          title: 'Forth Note',
+          body: 'Third tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices.',
+          createdAt: 1527789175390
+        },
+      ]
+    }
+  }
   render() {
     return (
       <div className='notelist-container'>
-        <div className='note'>
-          <h3>Title 1</h3>
-          <hr />
-          <p>Lorem ipsum dolor sit amet, oblique sententiae et per, liber utroque ponderum ut nec. Ei alienum adversarium his, et ius tota alterum nonumes. Nec error congue epicurei et. Te cum nostro aliquam vocibus, timeam qualisque no eum.</p>
-        </div>
-        <div className='note'>
-          <h3>Title 2</h3>
-          <hr />
-          <p>Lorem ipsum dolor sit amet, oblique sententiae et per, liber utroque ponderum ut nec. Ei alienum adversarium his, et ius tota alterum nonumes. Nec error congue epicurei et. Te cum nostro aliquam vocibus, timeam qualisque no eum.</p>
-        </div>
-        <div className='note'>
-          <h3>Title 3</h3>
-          <hr />
-          <p>Lorem ipsum dolor sit amet, oblique sententiae et per, liber utroque ponderum ut nec. Ei alienum adversarium his, et ius tota alterum nonumes. Nec error congue epicurei et. Te cum nostro aliquam vocibus, timeam qualisque no eum.</p>
-        </div>
-        <div className='note'>
-          <h3>Title 4</h3>
-          <hr />
-          <p>Lorem ipsum dolor sit amet, oblique sententiae et per, liber utroque ponderum ut nec. Ei alienum adversarium his, et ius tota alterum nonumes. Nec error congue epicurei et. Te cum nostro aliquam vocibus, timeam qualisque no eum.</p>
-        </div>
-        <div className='note'>
-          <h3>Title 5</h3>
-          <hr />
-          <p>Lorem ipsum dolor sit amet, oblique sententiae et per, liber utroque ponderum ut nec. Ei alienum adversarium his, et ius tota alterum nonumes. Nec error congue epicurei et. Te cum nostro aliquam vocibus, timeam qualisque no eum.</p>
-        </div>
-        <div className='note'>
-          <h3>Title 6</h3>
-          <hr />
-          <p>Lorem ipsum dolor sit amet, oblique sententiae et per, liber utroque ponderum ut nec. Ei alienum adversarium his, et ius tota alterum nonumes. Nec error congue epicurei et. Te cum nostro aliquam vocibus, timeam qualisque no eum.</p>
-        </div>
+        {this.state.notesArray.map((note, index) => {
+          return (
+            <div className='note' key={index + note.title}>
+              <h3>{note.title}</h3>
+              <hr />
+              <p>{note.body}</p>
+            </div>
+          )
+        })}          
+      
       </div>
     )
   }
